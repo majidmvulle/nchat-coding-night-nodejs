404e93f4cc93ea77eace9cec42288b58c725aa6c
Mostafa Hussien <mostafahussien@Mostafas-MBP.localdomain>
2020-03-09 15:32:12 +0400

    fe wip

--- src/frontend/src/pages/ChatRoom/ChatBox/ChatBox.tsx
+++ src/frontend/src/pages/ChatRoom/ChatBox/ChatBox.tsx
@@ -1,38 +1,20 @@
 import React, { useState } from "react";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
-import Avatar from "@material-ui/core/Avatar";
-import Chip from "@material-ui/core/Chip";
-import Moment from "react-moment";
+import Message from "../../../components/Message/Message";
 
 export interface ChatBoxProps {
   sendMessage: Function;
   messages: Array<string>;
 }
 
-const ChatBox: React.SFC<ChatBoxProps> = props => {
+const ChatBox: React.FC<ChatBoxProps> = props => {
   const [message, setMessage] = useState("");
   return (
     <div className="chatArea">
       <Box className="chatMessages">
         <Box className="topBar">13 people joined the room</Box>
-        {props.messages && props.messages.length === 0 && (
-          <Box textAlign="center" fontSize={20}>
-            There are no messages at the moment.
-          </Box>
-        )}
-        {props.messages &&
-          props.messages.length > 0 &&
-          props.messages.map((message, index) => {
-            return (
-              <Box key={index} mb={2}>
-                <Chip avatar={<Avatar>M</Avatar>} label={message} />
-                <Moment className="time" fromNow>
-                  2020-02-19T12:59-0500
-                </Moment>
-              </Box>
-            );
-          })}
+        <Message messages={props.messages} />
       </Box>
 
       <Box className="chatTextarea">
